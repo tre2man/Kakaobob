cc542620360ed5d3c72b51376e60490329d3c81f
tre2man <kimtree97@naver.com>
2020-02-20 23:58:57 +0900

    quickReplies 추가 및 기능 개선

--- bob.py
+++ bob.py
@@ -1,3 +1,4 @@
+
 from flask import Flask,request,jsonify
 import bs4
 import urllib.request
@@ -7,12 +8,9 @@ import time
 
 app = Flask(__name__)
 
-Restaurant=['학생식당','교직원 식당','푸름관','오름1동','오름3동']
-Time=['조식','중식','석식']
-Days=['오늘','월요일','화요일','수요일','목요일','금요일'',토요일','일요일']
-Reset='처음으로'
-
 #식당성정->날짜설정->아침점심저녁 설정->처음으로
+
+Restaurant=["학생식당","푸름관","오름1동","오름3동","교직원 식당"]
 
 ChoiceUrl=""
 ChoiceDay=0
@@ -25,25 +23,67 @@ urlorum1="http://dorm.kumoh.ac.kr/dorm/restaurant_menu02.do"
 urlorum3="http://dorm.kumoh.ac.kr/dorm/restaurant_menu03.do"
 
 '''
+
 월요일~일요일 중식 : 0~6
 월요일~일요일 석식 : 7~13
-@@@ 예외적으로 오름 1동은 중식->석식 @@@
+
+@@@ 예외적으로 오름 1동은 중식->조식 @@@
+
 '''
+
+jsonChoiceDay = {
+    "version": "2.0",
+    "template": {"outputs": [{"simpleText": {"text": "날짜를 선택해 주세요"}}],
+                 "quickReplies": [{"label": "오늘", "action": "message", "messageText": "오늘"},
+                                  {"label": "월", "action": "message", "messageText": "월"},
+                                  {"label": "화", "action": "message", "messageText": "화"},
+                                  {"label": "수", "action": "message", "messageText": "수"},
+                                  {"label": "목", "action": "message", "messageText": "목"},
+                                  {"label": "금", "action": "message", "messageText": "금"},
+                                  {"label": "토", "action": "message", "messageText": "토"},
+                                  {"label": "일", "action": "message", "messageText": "일"}]}
+}
+
+jsonChoiceRes = {
+    "version": "2.0",
+    "template": {
+        "outputs": [{"simpleText": {"text": "식당을 선택해 주세요"}}],
+        "quickReplies": [{"label": "학생식당", "action": "message", "messageText": "학생식당"},
+                         {"label": "푸름관", "action": "message", "messageText": "푸름관"},
+                         {"label": "오름1동", "action": "message", "messageText": "오름1동"},
+                         {"label": "오름3동", "action": "message", "messageText": "오름3동"},
+                         {"label": "교직원", "action": "message", "messageText": "교직원"},
+                         ]}
+}
+
+jsonChoiceTime = {
+    "version": "2.0",
+    "template": {
+        "outputs": [{"simpleText": {"text": "시간을 선택해 주세요"}}],
+        "quickReplies": [{"label": "아침", "action": "message", "messageText": "아침"},
+                         {"label": "점심", "action": "message", "messageText": "점심"},
+                         {"label": "저녁", "action": "message", "messageText": "저녁"}, ]}
+}
+
 
 def returnMenu(url,num):  #식단을 보여줄수 있게 하는 함수 (링크,식단종류)
     html = bs4.BeautifulSoup(urllib.request.urlopen(url), "html.parser")
-    menu = html.findAll("ul", {"class": "s-dot"})
-    return menu[num].text
+    menus=html.find("td")
+    menu=str(menus.text)  #bs4 자료형을 String 형태로 변환, 식단의 존재 유무 판별
+
+    if(menu=="등록된 메뉴가 없습니다."): #식단이 없을경우
+        return menu
+    else:                              #식단이 있을경우
+        html = bs4.BeautifulSoup(urllib.request.urlopen(url), "html.parser")
+        menu = html.findAll("ul", {"class": "s-dot"})
+        return menu[num].text
 
 
 @app.route('/keyboard')  #최초로 채팅방에 접속시 보여 줄 버튼
 def keyboard():
-    return jsonify({
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" : "안녕하세"}]}}]}
-            })
+    return jsonify({})
 
-@app.route('/message', methods=['POST'])  #json으로 들어온 사용자 요청을 보고 판단,테스트로 저녁메뉴만 봅시다
+@app.route('/message', methods=['POST'])  #json으로 들어온 사용자 요청을 보고 판단
 def bob():
 
     content = request.get_json() #사용자가 보낸 메세지 입력
@@ -53,135 +93,112 @@ def bob():
     global ChoiceUrl
     global ChoiceDay
     global ChoiceRes
+    global jsonChoiceDay
+    global jsonChoiceRes
+    global jsonChoiceTime
 
-    today=time.localtime().tm_wday+7 #저녁일 경우에는 +7
+    today=time.localtime().tm_wday #저녁일 경우에는 +7
 
-    if content==u"선택":
-        response_data={
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" : returnMenu(urlPorum,today)}]}}]}
-            }
+
+    if content==u"학생식당":
+        response_data=jsonChoiceDay
+        ChoiceUrl=urlStudent
+        ChoiceRes=0
 
     elif content==u"푸름관":
-        response_data={
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" : returnMenu(urlPorum,today)}]}}]}
-            }
-
-    elif content==u"교직원":
-        response_data={
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" : returnMenu(urlProfess,today)}]}}]}
-            }
-
-    elif content==u"오름1동":
-        response_data={
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" : returnMenu(urlorum1,today)}]}}]}
-            }
-
-    elif content==u"오름3동":
-        response_data={
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" : returnMenu(urlorum3,today)}]}}]}
-            }
-
-    elif content==u"학생식당":
-        response_data={
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" : returnMenu(urlStudent,today)}]}}]}
-            }
-
-
-    elif content==u"안녕":
-        response_data={
-            "version": "2.0",
-            "template": {"outputs": [{"carousel": {"type" : "basicCard","items": [{"title" : "","description" :"안녕"}]}}]}
-            }
-
-    '''
-
-    if user_input == Reset:  # 맨 마지막에서 다시 처음으로 올 때
-        response_data = {'message': {"text": "식당을 선택해 주세요"}, "keyboard": {"buttons": Restaurant, "type": "buttons", }}
-
-    elif user_input==Restaurant[0]:   #학생식당 선택할 경우
-        response_data={'message':{"text":"날짜를 선택해 주세요"},"keyboard" : {"buttons" : Days,"type" : "buttons",}}
-        ChoiceUrl=urlStudent
-        ChoiceRes = 0
-
-    elif user_input==Restaurant[1]:   #교직원식당 선택할 경우
-        response_data={'message':{"text":"날짜를 선택해 주세요"},"keyboard" : {"buttons" : Days,"type" : "buttons",}}
-        ChoiceUrl=urlProfess
+        print("푸름관")
+        response_data=jsonChoiceDay
+        ChoiceUrl=urlPorum
         ChoiceRes = 1
 
-    elif user_input==Restaurant[2]:   #푸름1 선택할 경우
-        response_data={'message':{"text":"날짜를 선택해 주세요"},"keyboard" : {"buttons" : Days,"type" : "buttons",}}
-        ChoiceUrl=urlPorum
+    elif content==u"오름1동":
+        response_data=jsonChoiceDay
+        ChoiceUrl=urlorum1
         ChoiceRes = 2
 
-    elif user_input==Restaurant[3]:   #오름1 선택할 경우
-        response_data={'message':{"text":"날짜를 선택해 주세요"},"keyboard" : {"buttons" : Days,"type" : "buttons",}}
-        ChoiceUrl=urlorum1
+    elif content == u"오름3동":
+        response_data=jsonChoiceDay
+        ChoiceUrl=urlorum3
         ChoiceRes = 3
 
-    elif user_input==Restaurant[4]:   #오름3 선택할 경우
-        response_data={'message':{"text":"날짜를 선택해 주세요"},"keyboard" : {"buttons" : Days,"type" : "buttons",}}
-        ChoiceUrl=urlorum3
+    elif content==u"교직원":
+        response_data=jsonChoiceDay
+        ChoiceUrl=urlProfess
         ChoiceRes = 4
 
-    elif user_input==Days[0]:   #오늘 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=time.localtime().tm_wday
+    elif content==u"오늘":
+        response_data=jsonChoiceTime
+        ChoiceDay = time.localtime().tm_wday
 
-    elif user_input==Days[1]:   #월요일 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=0
-
-    elif user_input==Days[2]:   #화요일 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=1
-
-    elif user_input==Days[3]:   #수요일 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=2
-
-    elif user_input==Days[4]:   #목요일 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=3
-
-    elif user_input==Days[5]:   #금요일 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=4
-
-    elif user_input==Days[6]:   #토요일 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=5
-
-    elif user_input==Days[7]:   #일요일 선택한 경우
-        response_data={'message':{"text":"시간을 선택해 주세요"},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        ChoiceDay=6
-
-    elif user_input==Time[0]:   #조식 선택한 경우
-        if ChoiceUrl!=urlorum1 :  #오름 1동 외에 나머지를 선택
-            response_data={'message':{"text": Restaurant[ChoiceRes]+"은 조식이 없습니다. 다시 선택해 주세요."},"keyboard" : {"buttons" : Time,"type" : "buttons",}}
-        else:                     #오름 1동&&조식
-            response_data={'message': {"text": returnMenu(ChoiceUrl,ChoiceDay)}, "keyboard": {"buttons": Reset, "type": "buttons", }}
-            ChoiceDay=0
-            ChoiceUrl=""
-
-    elif user_input==Time[1]:   #중식 선택한 경우
-        response_data={'message': {"text": returnMenu(ChoiceUrl,ChoiceDay)}, "keyboard": {"buttons": Reset, "type": "buttons", }}
+    elif content==u"월":
+        response_data=jsonChoiceTime
         ChoiceDay = 0
-        ChoiceUrl = ""
 
-    elif user_input==Time[2]:   #석식 선택한 경우
-        ChoiceDay+=7
-        response_data={'message': {"text": returnMenu(ChoiceUrl,ChoiceDay)}, "keyboard": {"buttons": Reset, "type": "buttons", }}
-        
-    '''
+    elif content==u"화":
+        response_data = jsonChoiceTime
+        ChoiceDay = 1
+
+    elif content==u"수":
+        response_data = jsonChoiceTime
+        ChoiceDay = 2
+
+    elif content==u"목":
+        response_data = jsonChoiceTime
+        ChoiceDay = 3
+
+    elif content==u"금":
+        response_data = jsonChoiceTime
+        ChoiceDay = 4
+
+    elif content==u"토":
+        response_data = jsonChoiceTime
+        ChoiceDay = 5
+
+    elif content==u"일":
+        response_data = jsonChoiceTime
+        ChoiceDay = 6
+
+    elif content==u"아침":
+        if(ChoiceUrl==urlorum1):  #오름1동 아침일경우 정상 출력
+            response_data={
+            "version": "2.0",
+            "template": {
+                "outputs": [{"carousel": {"type": "basicCard", "items": [{"title": "", "description": returnMenu(ChoiceUrl,ChoiceDay)}]}}],
+                "quickReplies": [{"label": "처음으로", "action": "message", "messageText": "처음으로"},]}
+            }
+        else:   #오름1동 아침이 아닐경우 경고 메시지 출력
+            response_data = {
+                "version": "2.0",
+                "template": {
+                    "outputs": [
+                        {"carousel": {"type": "basicCard", "items": [{"title": "", "description": Restaurant[ChoiceRes]+"은 아침이 없습니다. 다시 선택해 주세요."}]}}],
+                    "quickReplies": [{"label": "아침", "action": "message", "messageText": "아침"},
+                                     {"label": "점심", "action": "message", "messageText": "점심"},
+                                     {"label": "저녁", "action": "message", "messageText": "저녁"}, ]}
+            }
+
+    elif content==u"점심":
+        response_data={
+        "version": "2.0",
+        "template": {
+            "outputs": [{"carousel": {"type": "basicCard", "items": [{"title": "", "description": returnMenu(ChoiceUrl,ChoiceDay)}]}}],
+            "quickReplies": [{"label": "처음으로", "action": "message", "messageText": "처음으로"},]}
+        }
+
+    elif content==u"저녁":
+        response_data={
+        "version": "2.0",
+        "template": {
+            "outputs": [{"carousel": {"type": "basicCard", "items": [{"title": "", "description": returnMenu(ChoiceUrl,ChoiceDay+7)}]}}],
+            "quickReplies": [{"label": "처음으로", "action": "message", "messageText": "처음으로"},]}
+        }
+
+    elif content==u"처음으로":
+        response_data=jsonChoiceRes
 
     return jsonify(response_data)
 
-
 if __name__=="__main__":
      app.run(host="0.0.0.0", port=5000)
+
+
