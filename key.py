88cbad27b1d0207d0e150ff1468ef01778b14eaa
tre2man <45779423+tre2man@users.noreply.github.com>
2020-03-24 18:03:55 +0900

    Delete key.py

--- key.py
+++
@@ -1,9 +0,0 @@
-city = "37050" #구미시 코드
-key = "kb6LBBw2Jq1cFghDqy8h226dMGNgKfToicbxAfJI6KHpPZXpTqA76sygUnxxqnuUx984VWbCQbqQ05lXS0apPw%3D%3D" #개인 코드
-bustop = "GMB132" #금오공대 종점
-
-urlBustop = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList?serviceKey="+ key +"&cityCode=" + city + "&nodeId=" + bustop
-#금오공대종점 버스정류장 버스정보 url
-
-urlBusEnd = "http://openapi.tago.go.kr/openapi/service/BusRouteInfoInqireService/getRouteNoList?serviceKey="+ key +"&cityCode=" + city + "&routeNo="
-#버스정보 수집 urlBusEnd+버스번호
