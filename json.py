f65c3fb3c62b7edd60071a732f6ec3e0a29defac
tre2man <kimtree97@naver.com>
2020-03-25 00:58:14 +0900

    코드 정리
    코드 정리

---
+++ json.py
@@ -0,0 +1,140 @@
+from flask import Flask,request,jsonify
+import time
+import openpyxl as xl
+
+def returnMain():
+
+    jsonMainmenu = {
+        "version": "2.0",
+        "template": {"outputs": [{"simpleText": {"text": "원하시는 기능을 선택해 주세요"}}],
+                     "quickReplies": [{"label": "식단 정보", "action": "message", "messageText": "식단 정보"},
+                                      {"label": "날씨 정보", "action": "message", "messageText": "날씨 정보"},
+                                      {"label": "식당 이용 가능 시간", "action": "message", "messageText": "식당 이용 가능 시간"}
+                                      ]
+                     }
+    }
+
+    return jsonMainmenu
+
+
+def returnChoiceRes():
+
+    jsonChoiceRes = {
+        "version": "2.0",
+        "template": {"outputs": [{"simpleText": {"text": "🍽 식당을 선택해 주세요. 🍽"}}],
+                     "quickReplies": [{"label": "학생식당", "action": "message", "messageText": "학생식당"},
+                                      {"label": "푸름관", "action": "message", "messageText": "푸름관"},
+                                      {"label": "오름1동", "action": "message", "messageText": "오름1동"},
+                                      {"label": "오름3동", "action": "message", "messageText": "오름3동"},
+                                      {"label": "교직원", "action": "message", "messageText": "교직원"},
+                                      {"label": "분식당", "action": "message", "messageText": "분식당"}
+                                      ]
+                     }
+    }
+
+    return jsonChoiceRes
+
+
+def returnAvailableTimeChoice():
+
+    jsonChoiceAvailableTime = {
+        "version": "2.0",
+        "template": {"outputs": [{"simpleText": {"text": "식당을 선택해 주세요."}}],
+                     "quickReplies": [{"label": "학생식당", "action": "message", "messageText": "학생식당 시간"},
+                                      {"label": "기숙사", "action": "message", "messageText": "기숙사 시간"},
+                                      {"label": "교직원", "action": "message", "messageText": "교직원 시간"},
+                                      ]
+                     }
+    }
+
+    return jsonChoiceAvailableTime
+
+
+def returnMenujson(res,week):  #식당 메뉴를 json으로 리턴하는 함수
+
+    f = xl.load_workbook('files/menu.xlsx', data_only=True)
+    file = f['Sheet']
+
+    temp = {
+        "version": "2.0",
+        "template": {"outputs": [{"simpleText": {"text": file.cell(res+1,week+1).value}}],
+                     "quickReplies": [{"label": "처음으로", "action": "message", "messageText": "처음으로"},
+                                      ]
+                     }
+        }
+
+    return temp
+
+
+def returnAvaliableTime(index):  #식당 이용 가능 시간을 json으로 리턴하는 함수
+
+    temp = {
+         "version": "2.0",
+         "template": {
+             "outputs": [{"simpleText": {"text": index}}],
+             "quickReplies": [{"label": "처음으로", "action": "message", "messageText": "처음으로"},
+                              ]
+                     }
+         }
+
+    return temp
+
+
+def returnjsonChoiceday():  #날짜 선택지를 json으로 리턴하는 함수
+
+    temp = {
+        "version": "2.0",
+        "template": {"outputs": [{"simpleText": {
+            "text": "📅 요일을 선택해 주세요. 📅\n\n오늘은 " + str(time.localtime().tm_year) + "년 " + str(
+                time.localtime().tm_mon) + "월 " + str(time.localtime().tm_mday) + "일 " + week[
+                        time.localtime().tm_wday] + " 입니다."}}],
+                     "quickReplies": [{"label": "오늘", "action": "message", "messageText": "오늘"},
+                                      {"label": "월요일", "action": "message", "messageText": "월요일"},
+                                      {"label": "화요일", "action": "message", "messageText": "화요일"},
+                                      {"label": "수요일", "action": "message", "messageText": "수요일"},
+                                      {"label": "목요일", "action": "message", "messageText": "목요일"},
+                                      {"label": "금요일", "action": "message", "messageText": "금요일"},
+                                      {"label": "토요일", "action": "message", "messageText": "토요일"},
+                                      {"label": "일요일", "action": "message", "messageText": "일요일"}
+                                      ]
+                     }
+    }
+
+    return temp
+
+
+def returnWeatherjson():  #날씨 데이터를 json으로 반환하는 함수
+
+    f = xl.load_workbook('files/weather.xlsx', data_only=True)
+    file = f['Sheet']
+
+    temp = {
+              "version": "2.0",
+              "template": {
+                "outputs": [
+                  {
+                    "carousel": {
+                      "type": "basicCard",
+                      "items": [
+                        {
+                          "title": "오늘 날씨",
+                          "description":  f"현재 온도 : {file.cell(1,1).value}\n오늘 최저/최고 기온 : {file.cell(1,2).value}/{file.cell(1,3).value}\n"
+                                          f"미세먼지 : {file.cell(1,4).value}\n초미세먼지 : {file.cell(1,5).value}\n오존 : {file.cell(1,6).value}"
+                        },
+                        {
+                          "title": "내일 날씨",
+                          "description": f"내일 최저/최고 기온 : {file.cell(2,3).value}\n내일 오전/오후 강수 확률 : {file.cell(2,1).value} % / {file.cell(2,2).value} %"
+                        },
+                        {
+                          "title": "모레 날씨",
+                          "description": f"모레 최저/최고 기온 : {file.cell(3,3).value}\n모레 오전/오후 강수 확률 : {file.cell(3,1).value} % / {file.cell(3,2).value} %"
+                        }
+                      ]
+                    }
+                  }
+              ],
+                  "quickReplies": [{"label": "처음으로", "action": "message", "messageText": "처음으로"}]
+            }
+    }
+
+    return temp
